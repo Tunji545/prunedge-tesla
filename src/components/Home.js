8d7e6beb4fc563ef82cbbab05d5028bb50b1b39d
Olatunji Omisakin <msknolatunji@gmail.com>
2021-06-23 16:31:33 +0100

    Completed Desktop and Mobile version only fixed Nav

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import arrow from '../images/arrowhead.png';
 
 const Home = () => {
   return (
     <>
-      <div className='container img-bg1-home'>
+      <div className='img-bg1-home'>
         <h1 className='h1-home'>Model S</h1>
         <p className='p-home'>
           Order online for <span>touchless delivery</span>.
@@ -12,6 +13,63 @@ const Home = () => {
           <button className='bg-black'>custom order</button>
           <button>Existing Inventary</button>
         </div>
+        <img
+          src={arrow}
+          alt='arrowhead.png'
+          width='20'
+          height='20'
+          className='arrow'
+        />
+      </div>
+      <div className='img-bg2-home'>
+        <h1 className='h1Y-home'>Model Y</h1>
+        <p className='p-home'>
+          Order online for <span>touchless delivery</span>.
+        </p>
+        <div className='btnY-div'>
+          <button className='bg-black'>custom order</button>
+          <button>Existing Inventary</button>
+        </div>
+      </div>
+      <div className='img-bg3-home'>
+        <h1 className='h1-home'>Model 3</h1>
+        <p className='p-home'>
+          Order online for <span>touchless delivery</span>.
+        </p>
+        <div className='btn-div'>
+          <button className='bg-black'>custom order</button>
+          <button>Existing Inventary</button>
+        </div>
+      </div>
+      <div className='img-bg4-home'>
+        <h1 className='h1-home'>Model X</h1>
+        <p className='p-home'>
+          Order online for <span>touchless delivery</span>.
+        </p>
+        <div className='btn-div'>
+          <button className='bg-black'>custom order</button>
+          <button>Existing Inventary</button>
+        </div>
+      </div>
+      <div className='img-bg5-home'>
+        <h1 className='h1-home'>Solar Panels</h1>
+        <p className='p-home'>Lowest cost lowest panels in America</p>
+        <div className='btn-div'>
+          <button className='bg-black'>order now</button>
+          <button>learn more</button>
+        </div>
+      </div>
+      <div className='img-bg6-home'>
+        <h1 className='h1-home'>Solar Roofs</h1>
+        <p className='p-home'>Produce clean energy from your roof.</p>
+        <div className='btn-div'>
+          <button className='bg-black'>order now</button>
+          <button>learn more</button>
+        </div>
+      </div>
+      <div className='img-bg7-home'>
+        <h1 className='h7-home'>Accessories</h1>
+        <button className='bg7-black'>Shop Now</button>
       </div>
     </>
   );
